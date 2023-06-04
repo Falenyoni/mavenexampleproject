2a0c9dc566c1cf1232b4bbcfd9cd515902e8b2da
Bongani Nyoni <bonganinyoni@Bonganis-MacBook-Pro.local>
2023-06-04 17:04:30 +0200

    first commit

---
+++ src/main/java/com/example/mavenexampleproject/MavenexampleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenexampleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenexampleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavenexampleproject/MavenexampleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenexampleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
